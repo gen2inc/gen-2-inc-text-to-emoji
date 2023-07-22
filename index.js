068176f99cbf67f14f033cec9c1a869f474f657d
ellie <81702700+yeeterlol@users.noreply.github.com>
2023-07-22 15:58:18 -0700

    hello everynyan
    how are u you fine thank u

---
+++ index.js
@@ -0,0 +1,55 @@
+import clipboard from "clipboardy";
+import promptSync from "prompt-sync";
+
+const prompt = promptSync();
+
+while (true) {
+    let text = prompt("What text do you want to convert to emoji text: ");
+    clipboard.writeSync(convertToText(text));
+    console.log("copied :3")
+}
+
+function convertToText(text) {
+    let customCharacters = {
+        " ": ":zuzzbet_46:", 
+        "?": ":zuzzbet_39:", 
+        "!": ":zuzzbet_40:",
+        "'": ":zuzzbet_41:"
+    };
+    let string = "";
+
+    for (let i = 0; i < text.length; i++) {
+        if (customCharacters[text[i]]) { 
+            string += customCharacters[text[i]];
+            continue;
+        }
+
+        let whatLetter = text[i].toLowerCase().charCodeAt(0) - 96;
+        // tries it the normal alphabet way
+        if (whatLetter >= 1 && whatLetter <= 26) {
+            if (text[i+1] === ":" || text[i-1] === ":") {
+                continue;
+            } else {
+                string += `:zuzzbet_${('0' + whatLetter).slice(-2)}:`;
+            }
+        }
+        // attempts to make it a cool lil custom emoji
+        switch(true) {
+            case text[i] === ":" && text[i+1] === "3": 
+                string += ":zuzzbet_42:";
+                break;
+            case text[i-1] === ">" && text[i] === ":" && text[i+1] === "3":
+                string += ":zuzzbet_43:";
+                break;
+            case text[i] === "D" && text[i+1] === ":":
+                string += ":zuzzbet_44:";
+                break;
+            case text[i] === ":" && text[i+1] === "D":
+                string += ":zuzzbet_45:";
+                break;
+            
+        }
+    }
+
+    return string;
+}
